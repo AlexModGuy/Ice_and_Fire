06cac9ec73b44f4b6b701a797be5167c4557c1db
Alex-the-666 <alex.rowlands@cox.net>
2016-06-12 08:49:39 -0700

    wait for forge

--- src/main/java/com/github/alexthe666/iceandfire/core/ModItems.java
+++ src/main/java/com/github/alexthe666/iceandfire/core/ModItems.java
@@ -1,5 +1,6 @@
 package com.github.alexthe666.iceandfire.core;
 
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
@@ -68,11 +69,11 @@ public class ModItems {
 	public static Item dragon_skull;
 	public static Item dragon_armor_iron;
 
-	public static ArmorMaterial silverMetal = EnumHelper.addArmorMaterial("Silver", "iceandfire:armor_silverMetal", 25, new int[] { 2, 7, 6, 2 }, 20, null);
-	public static ArmorMaterial red = EnumHelper.addArmorMaterial("DragonRed", "iceandfire:armor_red", 25, new int[] { 2, 7, 6, 2 }, 15, null);
-	public static ArmorMaterial bronze = EnumHelper.addArmorMaterial("DragonBronze", "iceandfire:armor_bronze", 25, new int[] { 2, 7, 6, 2 }, 15, null);
-	public static ArmorMaterial green = EnumHelper.addArmorMaterial("DragonGreen", "iceandfire:armor_green", 25, new int[] { 2, 7, 6, 2 }, 15, null);
-	public static ArmorMaterial gray = EnumHelper.addArmorMaterial("DragonGray", "iceandfire:armor_gray", 25, new int[] { 2, 7, 6, 2 }, 15, null);
+	public static ArmorMaterial silverMetal = EnumHelper.addArmorMaterial("Silver", "iceandfire:armor_silverMetal", 25, new int[] { 2, 7, 6, 2 }, 20, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN);
+	public static ArmorMaterial red = EnumHelper.addArmorMaterial("DragonRed", "iceandfire:armor_red", 25, new int[] { 2, 7, 6, 2 }, 15, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN);
+	public static ArmorMaterial bronze = EnumHelper.addArmorMaterial("DragonBronze", "iceandfire:armor_bronze", 25, new int[] { 2, 7, 6, 2 }, 15, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN);
+	public static ArmorMaterial green = EnumHelper.addArmorMaterial("DragonGreen", "iceandfire:armor_green", 25, new int[] { 2, 7, 6, 2 }, 15, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN);
+	public static ArmorMaterial gray = EnumHelper.addArmorMaterial("DragonGray", "iceandfire:armor_gray", 25, new int[] { 2, 7, 6, 2 }, 15, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN);
 	public static ToolMaterial silverTools = EnumHelper.addToolMaterial("Silver", 2, 460, 11.0F, 1.0F, 18);
 	public static ToolMaterial boneTools = EnumHelper.addToolMaterial("Dragonbone", 4, 1660, 10.0F, 4.0F, 22);
 
--- src/main/java/com/github/alexthe666/iceandfire/entity/EntityFireDragon.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntityFireDragon.java
@@ -3,7 +3,16 @@ package com.github.alexthe666.iceandfire.entity;
 import com.github.alexthe666.iceandfire.core.ModSounds;
 
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIFollowParent;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMate;
+import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAITempt;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryBasic;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
@@ -20,6 +29,19 @@ public class EntityFireDragon extends EntityDragonBase {
 		this.ignoreFrustumCheck = true;
 	}
 
+	protected void initEntityAI()
+    {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 2.0D));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.25D, Items.WHEAT, false));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.25D));
+        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+    }
+
+	
 	@Override
 	public String getTexture() {
 		if (this.getSleeping() == 1) {
