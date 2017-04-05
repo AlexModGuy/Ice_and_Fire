a93b6afe1b824d72ff6b9777fc47692b1f5523c2
Alex-the-666 <alex.rowlands@cox.net>
2017-04-05 09:14:35 -0700

    whoops

--- src/main/java/com/github/alexthe666/iceandfire/entity/EntityDragonBase.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntityDragonBase.java
@@ -1007,7 +1007,7 @@ public abstract class EntityDragonBase extends EntityTameable implements IAnimat
                 this.setFlying(false);
                 this.setHovering(false);
             }
-            if (!world.isRemote && this.getRNG().nextInt(1250) == 0 && !this.isFlying() && this.getPassengers().isEmpty() && !this.isChild() && !this.isHovering() && !this.isSleeping() && this.canMove() && this.onGround) {
+            if (!world.isRemote && this.getRNG().nextInt(1250) == 0 && !this.isSitting() && !this.isFlying() && this.getPassengers().isEmpty() && !this.isChild() && !this.isHovering() && !this.isSleeping() && this.canMove() && this.onGround) {
                 this.setHovering(true);
                 this.setSleeping(false);
                 this.setSitting(false);
