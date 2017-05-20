6be7a37a2465ea5079f5a7e643fa65f8e9d26fc7
Alex-the-666 <alex.rowlands@cox.net>
2017-05-20 12:28:35 -0700

    gravel-types fall fixing #230

--- src/main/java/com/github/alexthe666/iceandfire/block/BlockFallingGeneric.java
+++ src/main/java/com/github/alexthe666/iceandfire/block/BlockFallingGeneric.java
@@ -4,9 +4,12 @@ import com.github.alexthe666.iceandfire.IceAndFire;
 import net.minecraft.block.BlockFalling;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockFallingGeneric extends BlockFalling {
     public Item itemBlock;
@@ -25,17 +28,26 @@ public class BlockFallingGeneric extends BlockFalling {
 
     }
 
-    public BlockFallingGeneric(Material materialIn, String gameName, String name, float hardness, float resistance, SoundType sound) {
+    public BlockFallingGeneric(Material materialIn, String gameName, String name, String toolUsed, int toolStrength, float hardness, float resistance, SoundType sound, boolean slippery) {
         super(materialIn);
         this.setUnlocalizedName(name);
         this.setHardness(hardness);
         this.setResistance(resistance);
+        this.setHarvestLevel(toolUsed, toolStrength);
         this.setSoundType(sound);
         this.setCreativeTab(IceAndFire.TAB);
         setRegistryName(IceAndFire.MODID, gameName);
+        if (slippery) {
+            this.slipperiness = 0.98F;
+        }
         GameRegistry.register(this);
         GameRegistry.register(itemBlock = (new ItemBlock(this).setRegistryName(this.getRegistryName())));
 
     }
 
+    @SideOnly(Side.CLIENT)
+    public int getDustColor(IBlockState blkst)
+    {
+        return -8356741;
+    }
 }
