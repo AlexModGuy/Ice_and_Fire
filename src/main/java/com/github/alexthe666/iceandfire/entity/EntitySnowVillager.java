e931a559c8a89524de7da304a57073639d18c9c3
Alex-the-666 <alex.rowlands@cox.net>
2017-05-27 11:05:14 -0700

    villagers working again

--- src/main/java/com/github/alexthe666/iceandfire/entity/EntitySnowVillager.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntitySnowVillager.java
@@ -5,15 +5,20 @@ import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class EntitySnowVillager extends EntityVillager {
 
@@ -31,7 +36,7 @@ public class EntitySnowVillager extends EntityVillager {
     @Nullable
     public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData livingdata) {
         livingdata = super.onInitialSpawn(difficulty, livingdata);
-        ModVillagers.INSTANCE.setRandomProfession(this, this.world.rand);
+        this.setProfession(ModVillagers.INSTANCE.shaman);
         return livingdata;
     }
 
@@ -39,9 +44,65 @@ public class EntitySnowVillager extends EntityVillager {
         if (professionId > 2) {
             professionId = 2;
         }
-        super.setProfession(professionId);
+        this.dataManager.set(PROFFESSION(), Integer.valueOf(professionId));
+
     }
 
+    private DataParameter<Boolean> BABY(){
+        Field field = ReflectionHelper.findField(EntityAgeable.class, ObfuscationReflectionHelper.remapFieldNames(EntityAgeable.class.getName(), new String[]{"BABY", "field_184751_bv"}));
+        try {
+            Field modifier = Field.class.getDeclaredField("modifiers");
+            modifier.setAccessible(true);
+            modifier.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            DataParameter<Boolean> bay = (DataParameter<Boolean>)field.get(this);
+            return bay;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private DataParameter<Integer> PROFFESSION(){
+        Field field = ReflectionHelper.findField(EntityVillager.class, ObfuscationReflectionHelper.remapFieldNames(EntityVillager.class.getName(), new String[]{"PROFESSION", "field_184752_bw"}));
+        try {
+            Field modifier = Field.class.getDeclaredField("modifiers");
+            modifier.setAccessible(true);
+            modifier.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+            DataParameter<Integer> prof = (DataParameter<Integer>)field.get(this);
+            return prof;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Override
+    public void notifyDataManagerChange(DataParameter<?> key)
+    {
+        if (HAND_STATES.equals(key) && this.world.isRemote)
+        {
+            if (this.isHandActive() && this.activeItemStack.isEmpty())
+            {
+                this.activeItemStack = this.getHeldItem(this.getActiveHand());
+
+                if (!this.activeItemStack.isEmpty())
+                {
+                    this.activeItemStackUseCount = this.activeItemStack.getMaxItemUseDuration();
+                }
+            }
+            else if (!this.isHandActive() && !this.activeItemStack.isEmpty())
+            {
+                this.activeItemStack = ItemStack.EMPTY;
+                this.activeItemStackUseCount = 0;
+            }
+        }
+//field_184751_bv
+        if (BABY().equals(key))
+        {
+            this.setScaleForAge(this.isChild());
+        }
+//field_184752_bw
+
+    }
     public void onDeath(DamageSource cause) {
         if (cause.getEntity() != null && cause.getEntity() instanceof EntityZombie && (this.world.getDifficulty() == EnumDifficulty.NORMAL || this.world.getDifficulty() == EnumDifficulty.HARD)) {
             return;
@@ -67,7 +128,12 @@ public class EntitySnowVillager extends EntityVillager {
     public net.minecraftforge.fml.common.registry.VillagerRegistry.VillagerProfession getProfessionForge() {
         if (this.prof == null) {
             String p = this.getEntityData().getString("ProfessionName");
-            this.prof = ModVillagers.INSTANCE.professions.get(intFromProfesion(p));
+            if(p.isEmpty()){
+                this.prof = ModVillagers.INSTANCE.professions.get(this.getRNG().nextInt(3));
+
+            }else{
+                this.prof = ModVillagers.INSTANCE.professions.get(intFromProfesion(p));
+            }
             try {
                 ReflectionHelper.findField(EntityVillager.class, new String[]{"field_175563_bv", "careerId"}).set(this, 1);
             } catch (IllegalAccessException e) {
