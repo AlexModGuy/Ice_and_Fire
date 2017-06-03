133518980dc0c2255eb8e22f39a2d94ef87d6141
Alex-the-666 <alex.rowlands@cox.net>
2017-06-03 10:45:30 -0700

    1.1.1

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
@@ -47,11 +47,11 @@ import java.util.Random;
 public class IceAndFire {
     /*
     -Dfml.coreMods.load=com.github.alexthe666.iceandfire.access.IceAndFireForgeLoading
-    This is here because Idea keeps deleting the VM arguments...
+    This is here because Intellij keeps deleting the VM arguments...
      */
     public static final String MODID = "iceandfire";
-    public static final String VERSION = "1.1.0";
-    public static final String LLIBRARY_VERSION = "1.4.1";
+    public static final String VERSION = "1.1.1";
+    public static final String LLIBRARY_VERSION = "1.7.4";
     public static final String NAME = "Ice And Fire";
     @Instance(value = MODID)
     public static IceAndFire INSTANCE;
