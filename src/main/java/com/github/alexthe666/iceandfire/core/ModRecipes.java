f8f275dc2d2e8bbe81b36d0a5581d81e4c83df75
Sunconure11 <eyespot7b@aol.com>
2017-09-28 13:02:09 -0400

    the lid

--- src/main/java/com/github/alexthe666/iceandfire/core/ModRecipes.java
+++ src/main/java/com/github/alexthe666/iceandfire/core/ModRecipes.java
@@ -65,7 +65,6 @@ public class ModRecipes {
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.iron_hippogryph_armor, 1, 0), new Object[]{"YXY", Character.valueOf('X'), Items.IRON_HORSE_ARMOR, Character.valueOf('Y'), "feather"}));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.gold_hippogryph_armor, 1, 0), new Object[]{"YXY", Character.valueOf('X'), Items.GOLDEN_HORSE_ARMOR, Character.valueOf('Y'), "feather"}));
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModItems.diamond_hippogryph_armor, 1, 0), new Object[]{"YXY", Character.valueOf('X'), Items.DIAMOND_HORSE_ARMOR, Character.valueOf('Y'), "feather"}));
-		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(ModBlocks.jar, 1, 0), new Object[]{"YXY", "Y Y", "YYY", Character.valueOf('X'), "plankWood", Character.valueOf('Y'), "blockGlass"}));
 		addBanner("firedragon", new ItemStack(ModItems.dragon_skull, 1, 0));
 		addBanner("icedragon", new ItemStack(ModItems.dragon_skull, 1, 1));
 		GameRegistry.addSmelting(ModBlocks.silverOre, new ItemStack(ModItems.silverIngot), 1);
