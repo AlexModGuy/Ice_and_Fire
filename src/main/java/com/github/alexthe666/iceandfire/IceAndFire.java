3666e0e439f9eee0f774cc8af67a0dd5612b3fe9
Sunconure11 <eyespot7b@aol.com>
2017-09-28 06:26:06 -0400

    Some stuff

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
@@ -45,7 +45,7 @@ import java.util.Random;
 public class IceAndFire {
 
 	public static final String MODID = "iceandfire";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.3.0";
 	public static final String LLIBRARY_VERSION = "1.7.7";
 	public static final String NAME = "Ice And Fire";
 	@Instance(value = MODID)
