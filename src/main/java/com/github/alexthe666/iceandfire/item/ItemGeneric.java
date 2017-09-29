c668f7335da6a740ec8475f88eb156f9b8519a6d
Sunconure11 <eyespot7b@aol.com>
2017-09-28 22:31:17 -0400

    more pruning

--- src/main/java/com/github/alexthe666/iceandfire/item/ItemGeneric.java
+++ src/main/java/com/github/alexthe666/iceandfire/item/ItemGeneric.java
@@ -1,7 +1,6 @@
 package com.github.alexthe666.iceandfire.item;
 
 import com.github.alexthe666.iceandfire.IceAndFire;
-import com.github.alexthe666.iceandfire.core.ModAchievements;
 import com.github.alexthe666.iceandfire.core.ModItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
