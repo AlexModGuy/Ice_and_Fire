d5ffd449c427032d344537cdfaf78adf1f96f36c
Alex-the-666 <alex.rowlands@cox.net>
2017-10-15 12:43:28 -0700

    just a little big left

--- src/main/java/com/github/alexthe666/iceandfire/client/model/FireDragonTabulaModelAnimator.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/model/FireDragonTabulaModelAnimator.java
@@ -4,8 +4,8 @@ import com.github.alexthe666.iceandfire.client.model.util.EnumDragonAnimations;
 import com.github.alexthe666.iceandfire.client.model.util.IIceAndFireTabulaModelAnimator;
 import com.github.alexthe666.iceandfire.client.model.util.IceAndFireTabulaModel;
 import com.github.alexthe666.iceandfire.entity.EntityFireDragon;
+import net.ilexiconn.llibrary.LLibrary;
 import net.ilexiconn.llibrary.client.model.tools.AdvancedModelRenderer;
-import net.minecraft.util.math.MathHelper;
 
 public class FireDragonTabulaModelAnimator implements IIceAndFireTabulaModelAnimator<EntityFireDragon> {
 
@@ -16,8 +16,13 @@ public class FireDragonTabulaModelAnimator implements IIceAndFireTabulaModelAnim
         IceAndFireTabulaModel currentPose = null;
         IceAndFireTabulaModel animationPose = null;
         IceAndFireTabulaModel[] walkPoses = {EnumDragonAnimations.WALK1.firedragon_model, EnumDragonAnimations.WALK2.firedragon_model, EnumDragonAnimations.WALK3.firedragon_model, EnumDragonAnimations.WALK4.firedragon_model};
-        float cos = calcCos(entity.ticksExisted, 0F, 1) * 20;
-        System.out.println(cos);
+        int prevIndex = (entity.walkCycle / 10) - 1;
+        if (prevIndex < 0) {
+            prevIndex = 3;
+        }
+        IceAndFireTabulaModel prevPosition = walkPoses[prevIndex];
+        IceAndFireTabulaModel currentPosition = walkPoses[(entity.walkCycle / 10)];
+        float delta = (entity.walkCycle / 10.0F) % 1.0F + (LLibrary.PROXY.getPartialTicks() / 10.0F);
         for(AdvancedModelRenderer cube : model.getCubes().values()){
             if(currentPose != null){
                 if(!isPartEqual(cube, currentPose.getCube(cube.boxName))){
@@ -29,17 +34,30 @@ public class FireDragonTabulaModelAnimator implements IIceAndFireTabulaModelAnim
                     transitionTo(cube, animationPose.getCube(cube.boxName), entity.ticksExisted % 40, 40);
                 }
             }
+
+            float prevX = prevPosition.getCube(cube.boxName).rotateAngleX;
+            float prevY = prevPosition.getCube(cube.boxName).rotateAngleY;
+            float prevZ = prevPosition.getCube(cube.boxName).rotateAngleZ;
+            float x = currentPosition.getCube(cube.boxName).rotateAngleX;
+            float y = currentPosition.getCube(cube.boxName).rotateAngleY;
+            float z = currentPosition.getCube(cube.boxName).rotateAngleZ;
+
+            this.setRotateAngle(cube, prevX + delta * distance(prevX, x), prevY + delta * distance(prevY, y), prevZ + delta * distance(prevZ, z));
+
+            /*
             transitionTo(cube, walkPoses[0].getCube(cube.boxName), MathHelper.clamp(cos, 0, 10), 10);
             transitionTo(cube, walkPoses[1].getCube(cube.boxName), MathHelper.clamp(cos, 10, 20) - 10, 10);
             transitionTo(cube, walkPoses[2].getCube(cube.boxName), MathHelper.clamp(cos, 20, 30) - 20, 10);
             transitionTo(cube, walkPoses[3].getCube(cube.boxName), MathHelper.clamp(cos, 30, 40) - 30, 10);
-
+            */
         }
 
     }
 
-    private float calcCos(float ticksExisted, float xOffset, float yOffset){
-        return ((float)Math.cos(xOffset + Math.toRadians((ticksExisted * 5) % 360)) + yOffset);
+    public void setRotateAngle(AdvancedModelRenderer model, float x, float y, float z) {
+        model.rotateAngleX = x;
+        model.rotateAngleY = y;
+        model.rotateAngleZ = z;
     }
 
     private boolean isPartEqual(AdvancedModelRenderer original, AdvancedModelRenderer pose){
@@ -59,12 +77,12 @@ public class FireDragonTabulaModelAnimator implements IIceAndFireTabulaModelAnim
     }
 
     private void transitionAngles(AdvancedModelRenderer from, AdvancedModelRenderer to, float timer, float maxTime){
-        from.rotateAngleX += ((rotateAmount(from.rotateAngleX, to.rotateAngleX)) / maxTime) * timer;
-        from.rotateAngleY += ((rotateAmount(from.rotateAngleY, to.rotateAngleY)) / maxTime) * timer;
-        from.rotateAngleZ += ((rotateAmount(from.rotateAngleZ, to.rotateAngleZ)) / maxTime) * timer;
+        from.rotateAngleX += ((distance(from.rotateAngleX, to.rotateAngleX)) / maxTime) * timer;
+        from.rotateAngleY += ((distance(from.rotateAngleY, to.rotateAngleY)) / maxTime) * timer;
+        from.rotateAngleZ += ((distance(from.rotateAngleZ, to.rotateAngleZ)) / maxTime) * timer;
     }
 
-    private float rotateAmount(float rotateAngleFrom, float rotateAngleTo){
+    private float distance(float rotateAngleFrom, float rotateAngleTo) {
         double distance = Math.toDegrees(rotateAngleTo) - Math.toDegrees(rotateAngleFrom);
         double reverseDistance = Math.toDegrees(rotateAngleFrom) - Math.toDegrees(rotateAngleTo);
         float sub = rotateAngleTo - rotateAngleFrom;
--- src/main/java/com/github/alexthe666/iceandfire/entity/EntityFireDragon.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntityFireDragon.java
@@ -30,6 +30,7 @@ public class EntityFireDragon extends EntityDragonBase {
 	public static float[] growth_stage_3 = new float[]{7F, 12.5F};
 	public static float[] growth_stage_4 = new float[]{12.5F, 20F};
 	public static float[] growth_stage_5 = new float[]{20F, 30F};
+	public int walkCycle;
 
 	public EntityFireDragon(World worldIn) {
 		super(worldIn, EnumDiet.CARNIVORE, 1, 1 + IceAndFire.CONFIG.dragonAttackDamage, IceAndFire.CONFIG.dragonHealth * 0.04, IceAndFire.CONFIG.dragonHealth, 0.2F, 0.5F);
@@ -195,7 +196,12 @@ public class EntityFireDragon extends EntityDragonBase {
 	@Override
 	public void onLivingUpdate() {
 		super.onLivingUpdate();
-
+		walkCycle++;
+		if (walkCycle < 30) {
+			walkCycle++;
+		} else {
+			walkCycle = 0;
+		}
 		if (this.getAttackTarget() != null && !this.isSleeping()) {
 				if ((!attackDecision || this.isFlying())) {
 				shootFireAtMob(this.getAttackTarget());
