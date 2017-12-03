514d36b553eb51dffb9a6d6c0d8ba38332fb5415
Alexthe666 <alex.rowlands@cox.net>
2017-12-03 11:02:24 -0800

    hippogryphs use biome dictionary now

--- src/main/java/com/github/alexthe666/iceandfire/core/ModEntities.java
+++ src/main/java/com/github/alexthe666/iceandfire/core/ModEntities.java
@@ -7,6 +7,7 @@ import net.ilexiconn.llibrary.server.entity.EntityPropertiesHandler;
 import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 import java.util.List;
@@ -42,9 +43,8 @@ public class ModEntities {
 		if (IceAndFire.CONFIG.spawnHippogryphs) {
 			for (EnumHippogryphTypes type : EnumHippogryphTypes.values()) {
 				if (!type.developer) {
-					for (int i = 0; i < type.spawnBiomes.length; i++) {
-						Biome biome = Biome.getBiome(type.spawnBiomes[i]);
-						if (biome != null) {
+					for (Biome biome : Biome.REGISTRY) {
+						if (biome != null && BiomeDictionary.hasType(biome, BiomeDictionary.Type.HILLS)) {
 							List<Biome.SpawnListEntry> spawnList = biome.getSpawnableList(EnumCreatureType.CREATURE);
 							spawnList.add(new Biome.SpawnListEntry(EntityHippogryph.class, 5, 1, 1));
 						}
