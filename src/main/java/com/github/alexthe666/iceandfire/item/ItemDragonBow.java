2c16c8109d502a9ca9cf5660cb916a11e8bd687d
TheMachine02 <themachine02om@gmail.com>
2018-02-05 16:53:47 +0100

    Fix dragon bow enchants
    It may be a bit dirty fix, but extending the class to ItemBow allow it to handle proprely the bow on enchant side with no side effect.

--- src/main/java/com/github/alexthe666/iceandfire/item/ItemDragonBow.java
+++ src/main/java/com/github/alexthe666/iceandfire/item/ItemDragonBow.java
@@ -17,7 +17,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class ItemDragonBow extends Item {
+public class ItemDragonBow extends ItemBow {
 
 	public ItemDragonBow() {
 		this.maxStackSize = 1;
