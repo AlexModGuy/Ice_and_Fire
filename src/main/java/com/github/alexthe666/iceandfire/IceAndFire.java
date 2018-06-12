ce2db71662ec5beb418c77641db277aee7306db3
Alexthe666 <alex.rowlands@cox.net>
2018-06-12 12:50:21 -0700

    alpha 1.5.0

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
@@ -43,7 +43,7 @@ import java.util.Random;
 public class IceAndFire {
 
 	public static final String MODID = "iceandfire";
-	public static final String VERSION = "1.4.0";
+	public static final String VERSION = "1.5.0";
 	public static final String LLIBRARY_VERSION = "1.7.9";
 	public static final String NAME = "Ice And Fire";
 	public static final Logger logger = LogManager.getLogger(NAME);
