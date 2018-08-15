7b4ddd9ec4104744058a7cdacc5037f0dc44e589
Alexthe666 <alex.rowlands@cox.net>
2018-08-15 11:59:26 -0700

    basic food room

--- src/main/java/com/github/alexthe666/iceandfire/structures/WorldGenMyrmexHive.java
+++ src/main/java/com/github/alexthe666/iceandfire/structures/WorldGenMyrmexHive.java
@@ -17,6 +17,8 @@ import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.fml.common.IWorldGenerator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class WorldGenMyrmexHive extends WorldGenerator {
@@ -39,7 +41,7 @@ public class WorldGenMyrmexHive extends WorldGenerator {
         IBlockState sticky_resin = isJungleBiome(world, position) ? STICKY_JUNGLE_RESIN : STICKY_DESERT_RESIN;
         generateSphere(world, rand, position, 14, 7, resin, sticky_resin);
         generateSphere(world, rand, position, 12, 5, Blocks.AIR.getDefaultState());
-
+        decorateSphere(world, rand, position, 12, 5, RoomType.QUEEN);
         generatePath(world, rand, position.offset(EnumFacing.NORTH, 9), 15 + rand.nextInt(10), EnumFacing.NORTH, 100);
         generatePath(world, rand, position.offset(EnumFacing.SOUTH, 9), 15 + rand.nextInt(10), EnumFacing.SOUTH, 100);
         generatePath(world, rand, position.offset(EnumFacing.WEST, 9), 15 + rand.nextInt(10), EnumFacing.WEST, 100);
@@ -67,6 +69,7 @@ public class WorldGenMyrmexHive extends WorldGenerator {
         IBlockState sticky_resin = isJungleBiome(world, position) ? STICKY_JUNGLE_RESIN : STICKY_DESERT_RESIN;
         generateSphere(world, rand, position, size + 2, height + 2, resin, sticky_resin);
         generateSphere(world, rand, position, size, height - 1, Blocks.AIR.getDefaultState());
+        decorateSphere(world, rand, position, size, height - 1, RoomType.random(rand));
         if(rand.nextInt(3) == 0 && direction.getOpposite() != EnumFacing.NORTH){
             generatePath(world, rand, position.offset(EnumFacing.NORTH, size - 2), 5 + rand.nextInt(20), EnumFacing.NORTH, roomChance);
         }
@@ -159,14 +162,68 @@ public class WorldGenMyrmexHive extends WorldGenerator {
         }
     }
 
+    private void decorateSphere(World world, Random rand, BlockPos position, int size, int height, RoomType roomType) {
+        int i2 = size;
+        int ySize = rand.nextInt(2);
+        int j = i2 + rand.nextInt(2);
+        int k = height + ySize;
+        int l = i2 + rand.nextInt(2);
+        float f = (float) (j + k + l) * 0.333F;
+        for (BlockPos blockpos : BlockPos.getAllInBox(position.add(-j, -k, -l), position.add(j, k, l))) {
+            if (blockpos.distanceSq(position) <= (double) (f * f * MathHelper.clamp(rand.nextFloat(), 0.75F, 1.0F)) && !world.isAirBlock(blockpos)) {
+                if(world.getBlockState(blockpos.down()).isFullCube() && world.isAirBlock(blockpos.up())){
+                    decorate(world, blockpos, position, size, rand, roomType);
+                }
+            }
+        }
+    }
+
+    private void decorate(World world, BlockPos blockpos, BlockPos center, int size, Random random, RoomType roomType) {
+        switch(roomType){
+            case FOOD:
+                if(random.nextInt(15) == 0){
+                    WorldGenMyrmexDecoration.generateSkeleton(world, blockpos, center, size, random);
+                }
+                if(random.nextInt(8) == 0){
+                    WorldGenMyrmexDecoration.generateLeaves(world, blockpos, center, size, random);
+                }
+                if(random.nextInt(8) == 0){
+                    WorldGenMyrmexDecoration.generatePumpkins(world, blockpos, center, size, random);
+                }
+                if(random.nextInt(2) == 0){
+                    WorldGenMyrmexDecoration.generateMushrooms(world, blockpos, center, size, random);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     private static boolean isJungleBiome(World world, BlockPos position) {
         Biome biome = world.getBiome(position);
         return biome.topBlock != Blocks.SAND && biome.fillerBlock != Blocks.SAND && !BiomeDictionary.hasType(biome, BiomeDictionary.Type.SANDY);
     }
 
     private enum RoomType{
-        DEFAULT,
-        FOOD,
-        EMPTY
+        DEFAULT(false),
+        QUEEN(false),
+        FOOD(true),
+        EMPTY(true),
+        NURSERY(true),
+        SHINY(true);
+        boolean random;
+
+        RoomType(boolean random){
+            this.random = random;
+        }
+        public static RoomType random(Random rand) {
+            List<RoomType> list = new ArrayList<RoomType>();
+            for(RoomType type : RoomType.values()){
+                if(type.random){
+                    list.add(type);
+                }
+            }
+            return list.get(rand.nextInt(list.size()));
+        }
     }
 }
