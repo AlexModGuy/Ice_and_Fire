9fbb767a24062547794ba348185eb7f23e02a0c0
Alex-the-666 <alex.rowlands@cox.net>
2018-12-24 11:39:56 -0800

    fixed pixie not taking sugar

--- src/main/java/com/github/alexthe666/iceandfire/entity/EntityPixie.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntityPixie.java
@@ -159,8 +159,15 @@ public class EntityPixie extends EntityTameable {
 			return true;
 		}
 		if (this.isOwner(player)) {
-			this.setSitting(!this.isSitting());
-			return true;
+			if(player.getHeldItem(hand).getItem() == Items.SUGAR){
+				this.heal(5);
+				player.getHeldItem(hand).shrink(1);
+				this.playSound(ModSounds.PIXIE_TAUNT, 1F, 1F);
+				return true;
+			}else{
+				this.setSitting(!this.isSitting());
+				return true;
+			}
 		} else if (player.getHeldItem(hand).getItem() == Item.getItemFromBlock(ModBlocks.jar_empty) && player.getHeldItem(hand).getMetadata() == 0 && !this.isTamed()) {
 			if (!player.isCreative()) {
 				player.getHeldItem(hand).shrink(1);
@@ -174,11 +181,6 @@ public class EntityPixie extends EntityTameable {
 			}
 			//player.addStat(ModAchievements.jarPixie);
 			this.setDead();
-		}else if(player.getHeldItem(hand).getItem() == Items.SUGAR){
-			this.heal(5);
-			player.getHeldItem(hand).shrink(1);
-			this.playSound(ModSounds.PIXIE_TAUNT, 1F, 1F);
-			return true;
 		}
 		return super.processInteract(player, hand);
 	}
