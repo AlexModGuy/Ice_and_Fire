19c4b09b1b6c5fd4bc838b3728a3f648592348f8
Alexthe666 <alex.rowlands@cox.net>
2019-01-11 20:58:39 -0800

    fixed #1186

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFireConfig.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFireConfig.java
@@ -205,7 +205,7 @@ public class IceAndFireConfig {
 		this.generateMyrmexColonies = config.getBoolean("Spawn Myrmex", "all", true, "True if myrmex colonies are allowed to spawn");
 		this.myrmexPregnantTicks = config.getInt("Myrmex Gestation Length", "all", 2500, 1, 10000, "How many ticks it takes for a Myrmex Queen to produce an egg.");
 		this.myrmexEggTicks = config.getInt("Myrmex Hatch Length", "all", 3000, 1, 10000, "How many ticks it takes for a Myrmex Egg to hatch.");
-		this.myrmexLarvaTicks = config.getInt("Myrmex Hatch Length", "all", 35000, 1, 10000, "How many ticks it takes for a Myrmex to move from a larva to a pupa, and from a pupa to an adult.");
+		this.myrmexLarvaTicks = config.getInt("Myrmex Hatch Length", "all", 35000, 1, 100000, "How many ticks it takes for a Myrmex to move from a larva to a pupa, and from a pupa to an adult.");
 		this.myrmexColonyGenChance = config.getInt("Myrmex Colony Gen Chance", "all", 150, 1, 10000, "One out of this number chance per chunk to generate a myrmex hive.");
 		this.experimentalPathFinder = config.getBoolean("Experimental Dragon path Finder", "all", false, "Turning this to true simplifies the dragon's pathfinding process, making them dumber when finding a path, but better for servers with many loaded dragons.");
 
