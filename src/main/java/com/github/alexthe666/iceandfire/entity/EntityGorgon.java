6d18cf6fed14727f534eaf54978fad819dd932f0
Alexthe666 <alex.rowlands@cox.net>
2019-03-13 20:31:16 -0700

    fix #1373

--- src/main/java/com/github/alexthe666/iceandfire/entity/EntityGorgon.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntityGorgon.java
@@ -179,10 +179,6 @@ public class EntityGorgon extends EntityMob implements IAnimatedEntity, IVillage
 
 	public void onLivingUpdate() {
 		super.onLivingUpdate();
-		//Temp fix for messed up structure spawn
-		if(this.getHealth() == 50 && this.ticksExisted == 1){
-			this.heal((float)IceAndFire.CONFIG.gorgonMaxHealth);
-		}
 		if (this.getAttackTarget() != null) {
 			boolean blindness = this.isPotionActive(MobEffects.BLINDNESS) || this.getAttackTarget().isPotionActive(MobEffects.BLINDNESS);
 			this.getLookHelper().setLookPosition(this.getAttackTarget().posX, this.getAttackTarget().posY + (double) this.getAttackTarget().getEyeHeight(), this.getAttackTarget().posZ, (float) this.getHorizontalFaceSpeed(), (float) this.getVerticalFaceSpeed());
