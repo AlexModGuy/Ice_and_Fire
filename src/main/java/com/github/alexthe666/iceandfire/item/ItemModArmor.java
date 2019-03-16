6f4965bdbe7ccf4ba0a81c83b40976ac16461e37
Alexthe666 <alex.rowlands@cox.net>
2019-03-16 14:09:20 -0700

    1.7.0 release

--- src/main/java/com/github/alexthe666/iceandfire/item/ItemModArmor.java
+++ src/main/java/com/github/alexthe666/iceandfire/item/ItemModArmor.java
@@ -1,12 +1,21 @@
 package com.github.alexthe666.iceandfire.item;
 
 import com.github.alexthe666.iceandfire.IceAndFire;
+import com.github.alexthe666.iceandfire.client.StatCollector;
 import com.github.alexthe666.iceandfire.core.ModItems;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreDictionary;
+
+import javax.annotation.Nullable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class ItemModArmor extends ItemArmor {
 
@@ -15,6 +24,28 @@ public class ItemModArmor extends ItemArmor {
 		this.setCreativeTab(IceAndFire.TAB);
 		this.setTranslationKey(name);
 		this.setRegistryName(IceAndFire.MODID, gameName);
+	}
+
+	public String getTranslationKey(ItemStack stack){
+		if(this == ModItems.earplugs) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(new Date());
+			if (calendar.get(2) + 1 == 4 && calendar.get(5) == 1) {
+				return "item.iceandfire.air_pods";
+			}
+		}
+		return super.getTranslationKey(stack);
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		if (this == ModItems.earplugs) {
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(new Date());
+			if (calendar.get(2) + 1 == 4 && calendar.get(5) == 1) {
+				tooltip.add(TextFormatting.GREEN + StatCollector.translateToLocal("item.iceandfire.air_pods.desc"));
+			}
+		}
 	}
 
 
