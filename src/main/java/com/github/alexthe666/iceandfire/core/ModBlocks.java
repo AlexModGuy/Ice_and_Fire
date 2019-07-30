575c8435238755b389631274e809c5720d14a6e2
Alexthe666 <alex.rowlands@cox.net>
2019-07-30 16:09:46 -0700

    dragonsteel blocks

--- src/main/java/com/github/alexthe666/iceandfire/core/ModBlocks.java
+++ src/main/java/com/github/alexthe666/iceandfire/core/ModBlocks.java
@@ -136,7 +136,10 @@ public class ModBlocks {
 	public static Block myrmex_desert_resin_glass = new BlockMyrmexConnectedResin(false, true);
 	@GameRegistry.ObjectHolder(IceAndFire.MODID + ":myrmex_jungle_resin_glass")
 	public static Block myrmex_jungle_resin_glass = new BlockMyrmexConnectedResin(true, true);
-
+	@GameRegistry.ObjectHolder(IceAndFire.MODID + ":dragonsteel_fire_block")
+	public static Block dragonsteel_fire_block = new BlockGeneric(Material.IRON, "dragonsteel_fire_block", "iceandfire.dragonsteel_fire_block", "pickaxe", 3, 10.0F, 1000.0F, SoundType.METAL);
+	@GameRegistry.ObjectHolder(IceAndFire.MODID + ":dragonsteel_ice_block")
+	public static Block dragonsteel_ice_block = new BlockGeneric(Material.IRON, "dragonsteel_ice_block", "iceandfire.dragonsteel_ice_block", "pickaxe", 3, 10.0F, 1000.0F, SoundType.METAL);
 	static {
 		GameRegistry.registerTileEntity(TileEntityDummyGorgonHead.class, "dummyGorgonHeadIdle");
 		GameRegistry.registerTileEntity(TileEntityDummyGorgonHeadActive.class, "dummyGorgonHeadActive");
