9a8e9399c8d89afc7ea51ed657538ce6fda88536
Alex-the-666 <alex.rowlands@cox.net>
2020-04-13 11:03:47 -0700

    fix #1907

--- src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAIStoreItems.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/ai/MyrmexAIStoreItems.java
@@ -56,7 +56,7 @@ public class MyrmexAIStoreItems extends EntityAIBase {
                 this.myrmex.getNavigator().tryMoveToXYZ(mainRoom.getX() + 0.5D, mainRoom.getY() + 0.5D, mainRoom.getZ() + 0.5D, this.movementSpeed);
 
             }
-            if (this.myrmex.getDistanceSq(mainRoom) < 8) {
+            if (this.myrmex.getDistanceSq(mainRoom) < 10D) {
                 first = false;
                 return;
             }
@@ -65,7 +65,7 @@ public class MyrmexAIStoreItems extends EntityAIBase {
             if(this.myrmex.getNavigator().noPath()) {
                 this.myrmex.getNavigator().tryMoveToXYZ(nextCocoon.getX() + 0.5D, nextCocoon.getY() + 0.5D, nextCocoon.getZ() + 0.5D, this.movementSpeed);
             }
-            if (this.myrmex.getDistanceSq(nextCocoon) < 4 && !this.myrmex.getHeldItem(EnumHand.MAIN_HAND).isEmpty() && isUseableCocoon(nextCocoon)) {
+            if (this.myrmex.getDistanceSq(nextCocoon) < 5.5D && !this.myrmex.getHeldItem(EnumHand.MAIN_HAND).isEmpty() && isUseableCocoon(nextCocoon)) {
                 TileEntityMyrmexCocoon cocoon = (TileEntityMyrmexCocoon) this.myrmex.world.getTileEntity(nextCocoon);
                 ItemStack itemstack = this.myrmex.getHeldItem(EnumHand.MAIN_HAND);
                 if (!itemstack.isEmpty()) {
