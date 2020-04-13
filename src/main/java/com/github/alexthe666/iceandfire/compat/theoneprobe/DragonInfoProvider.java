5d572f20aedc9bea79303fe6f8fbca5cb69f3c6c
Alex-the-666 <alex.rowlands@cox.net>
2020-04-13 10:41:51 -0700

    fix #2230

--- src/main/java/com/github/alexthe666/iceandfire/compat/theoneprobe/DragonInfoProvider.java
+++ src/main/java/com/github/alexthe666/iceandfire/compat/theoneprobe/DragonInfoProvider.java
@@ -9,6 +9,7 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
+import slimeknights.tconstruct.library.Util;
 
 public class DragonInfoProvider implements IProbeInfoEntityProvider {
 
@@ -21,13 +22,14 @@ public class DragonInfoProvider implements IProbeInfoEntityProvider {
     public void addProbeEntityInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, Entity entity, IProbeHitEntityData data) {
         if (entity instanceof EntityDragonBase) {
             EntityDragonBase dragon = (EntityDragonBase) entity;
+
             if (dragon.isTamed() && dragon.getOwner() != null) {
-                probeInfo.horizontal().text(I18n.format("dragon.owner") + dragon.getOwner().getName());
+                probeInfo.horizontal().text(Util.translateFormatted("dragon.owner") + dragon.getOwner().getName());
             } else {
-                probeInfo.horizontal().text(I18n.format("dragon.untamed"));
+                probeInfo.horizontal().text(Util.translateFormatted("dragon.untamed"));
             }
-            probeInfo.horizontal().text(I18n.format("dragon.gender") + I18n.format(dragon.isMale() ? "dragon.gender.male" : "dragon.gender.female"));
-            probeInfo.horizontal().text(I18n.format("dragon.stage") + dragon.getDragonStage());
+            probeInfo.horizontal().text(Util.translateFormatted("dragon.gender") + Util.translateFormatted(dragon.isMale() ? "dragon.gender.male" : "dragon.gender.female"));
+            probeInfo.horizontal().text(Util.translateFormatted("dragon.stage") + dragon.getDragonStage());
         }
     }
 }
