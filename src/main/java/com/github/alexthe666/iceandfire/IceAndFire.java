cc291166567d1d59b572ec90f65306719aa81195
Alex-the-666 <alex.rowlands@cox.net>
2020-05-01 13:47:53 -0700

    Update IceAndFire.java

--- src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
+++ src/main/java/com/github/alexthe666/iceandfire/IceAndFire.java
@@ -1,177 +1,88 @@
 package com.github.alexthe666.iceandfire;
 
-import com.github.alexthe666.iceandfire.compat.CraftTweakerCompatBridge;
-import com.github.alexthe666.iceandfire.compat.OneProbeCompatBridge;
-import com.github.alexthe666.iceandfire.compat.ThaumcraftCompatBridge;
-import com.github.alexthe666.iceandfire.compat.TinkersCompatBridge;
-import com.github.alexthe666.iceandfire.entity.IafEntityRegistry;
-import com.github.alexthe666.iceandfire.recipe.IafRecipeRegistry;
-import com.github.alexthe666.iceandfire.entity.IafVillagerRegistry;
-import com.github.alexthe666.iceandfire.world.IafWorldRegistry;
-import com.github.alexthe666.iceandfire.event.ServerEvents;
-import com.github.alexthe666.iceandfire.event.WorldGenEvents;
-import com.github.alexthe666.iceandfire.loot.CustomizeToDragon;
-import com.github.alexthe666.iceandfire.loot.CustomizeToSeaSerpent;
-import com.github.alexthe666.iceandfire.message.*;
-import com.github.alexthe666.iceandfire.misc.CreativeTab;
-import com.github.alexthe666.iceandfire.world.village.ComponentAnimalFarm;
-import com.github.alexthe666.iceandfire.world.village.MapGenSnowVillage;
-import com.github.alexthe666.iceandfire.world.village.VillageAnimalFarmCreator;
-import net.ilexiconn.llibrary.server.network.NetworkWrapper;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.world.gen.structure.MapGenStructureIO;
-import net.minecraft.world.storage.loot.functions.LootFunctionManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.config.Configuration;
-import net.minecraftforge.fml.common.Loader;
+
+import com.github.alexthe666.iceandfire.item.IafItemRegistry;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.common.registry.VillagerRegistry;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.network.NetworkDirection;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.util.Random;
+import java.util.logging.Logger;
 
-@Mod(modid = IceAndFire.MODID,
-        dependencies = "required-after:llibrary@[" + IceAndFire.LLIBRARY_VERSION + ",)",
-        version = IceAndFire.VERSION, name = IceAndFire.NAME, guiFactory = "com.github.alexthe666.iceandfire.client.gui.IceAndFireGuiFactory")
+@Mod(IceAndFire.MODID)
+@Mod.EventBusSubscriber(modid = IceAndFire.MODID)
 public class IceAndFire {
-
-    public static final String MODID = "iceandfire";
-    public static final String VERSION = "1.9.1";
-    public static final String LLIBRARY_VERSION = "1.7.9";
-    public static final String NAME = "Ice And Fire";
-    public static final Logger logger = LogManager.getLogger(NAME);
-    @Instance(value = MODID)
-    public static IceAndFire INSTANCE;
-    @NetworkWrapper({MessageDaytime.class, MessageDragonControl.class, MessageHippogryphArmor.class, MessageStoneStatue.class,
-            MessageUpdatePixieHouse.class, MessageUpdatePodium.class, MessageUpdatePixieHouseModel.class, MessageUpdatePixieJar.class, MessageSirenSong.class,
-            MessageDeathWormHitbox.class, MessageMultipartInteract.class, MessageGetMyrmexHive.class, MessageSetMyrmexHiveNull.class, MessagePlayerHitMultipart.class,
-            MessageAddChainedEntity.class, MessageRemoveChainedEntity.class, MessageDragonSetBurnBlock.class, MessageDragonSyncFire.class, MessageSpawnParticleAt.class,
-            MessageStartRidingMob.class})
-    public static SimpleNetworkWrapper NETWORK_WRAPPER;
-    @SidedProxy(clientSide = "com.github.alexthe666.iceandfire.ClientProxy", serverSide = "com.github.alexthe666.iceandfire.CommonProxy")
-    public static CommonProxy PROXY;
-    public static CreativeTabs TAB_ITEMS;
-    public static CreativeTabs TAB_BLOCKS;
-    public static DamageSource dragon;
-    public static DamageSource dragonFire;
-    public static DamageSource dragonIce;
-    public static DamageSource gorgon;
-    public static IceAndFireConfig CONFIG = new IceAndFireConfig();
-    public static Configuration config;
-    public static final boolean DEBUG = false;
-
-    public static void loadConfig() {
-        File configFile = new File(Loader.instance().getConfigDir(), "ice_and_fire.cfg");
-        if (!configFile.exists()) {
-            try {
-                configFile.createNewFile();
-            } catch (Exception e) {
-                logger.warn("Could not create a new Ice and Fire config file.");
-                logger.warn(e.getLocalizedMessage());
-            }
+    public static final Logger LOGGER = (Logger)LogManager.getLogger();
+    public static final String MODID = "rats";
+    public static final SimpleChannel NETWORK_WRAPPER;
+    private static final String PROTOCOL_VERSION = Integer.toString(1);
+    public static ItemGroup TAB_ITEMS = new ItemGroup(MODID) {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(IafItemRegistry.dragon_skull);
         }
-        config = new Configuration(configFile);
-        config.load();
+    };
+    public static ItemGroup TAB_BLOCKS = new ItemGroup("iceandfire.blocks") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(IafItemRegistry.dragonscales_red);
+        }
+    };
+    public static CommonProxy PROXY = DistExecutor.runForDist(() -> ClientProxy::new, () -> CommonProxy::new);
+    private static int packetsRegistered = 0;
+
+    static {
+        NetworkRegistry.ChannelBuilder channel = NetworkRegistry.ChannelBuilder.named(new ResourceLocation("iceandfire", "main_channel"));
+        String version = PROTOCOL_VERSION;
+        version.getClass();
+        channel = channel.clientAcceptedVersions(version::equals);
+        version = PROTOCOL_VERSION;
+        version.getClass();
+        NETWORK_WRAPPER = channel.serverAcceptedVersions(version::equals).networkProtocolVersion(() -> {
+            return PROTOCOL_VERSION;
+        }).simpleChannel();
     }
 
-    public static void syncConfig() {
-        CONFIG.init(config);
-        config.save();
-    }
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        loadConfig();
-        syncConfig();
-        MinecraftForge.EVENT_BUS.register(new ServerEvents());
-        TAB_ITEMS = new CreativeTab(MODID + "_items");
-        TAB_BLOCKS = new CreativeTab(MODID + "_blocks");
-        IafEntityRegistry.init();
-        IafWorldRegistry.init();
-        MinecraftForge.EVENT_BUS.register(PROXY);
-        logger.info("A raven flies from the north to the sea");
-        logger.info("A dragon whispers her name in the east");
-        ThaumcraftCompatBridge.loadThaumcraftCompat();
-        LootFunctionManager.registerFunction(new CustomizeToDragon.Serializer());
-        LootFunctionManager.registerFunction(new CustomizeToSeaSerpent.Serializer());
-        OneProbeCompatBridge.loadPreInit();
-        TinkersCompatBridge.loadTinkersCompat();
-        CraftTweakerCompatBridge.loadTweakerCompat();
-        PROXY.preRender();
-    }
-
-    @EventHandler
-    public void init(FMLInitializationEvent event) {
-        IafVillagerRegistry.INSTANCE.init();
-        logger.info("The watcher waits on the northern wall");
-        logger.info("A daughter picks up a warrior's sword");
-        MapGenStructureIO.registerStructure(MapGenSnowVillage.Start.class, "SnowVillageStart");
-        MapGenStructureIO.registerStructureComponent(ComponentAnimalFarm.class, "AnimalFarm");
-        VillagerRegistry.instance().registerVillageCreationHandler(new VillageAnimalFarmCreator());
+    public IceAndFire() {
+        final ModLoadingContext modLoadingContext = ModLoadingContext.get();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+        //modLoadingContext.registerConfig(ModConfig.Type.CLIENT, ConfigHolder.CLIENT_SPEC);
+        //modLoadingContext.registerConfig(ModConfig.Type.SERVER, ConfigHolder.SERVER_SPEC);
         PROXY.render();
-        GameRegistry.registerWorldGenerator(new WorldGenEvents(), 0);
-        NetworkRegistry.INSTANCE.registerGuiHandler(this, new com.github.alexthe666.iceandfire.client.GuiHandler());
-        dragon = new DamageSource("dragon") {
-            @Override
-            public ITextComponent getDeathMessage(EntityLivingBase entityLivingBaseIn) {
-                String s = "death.attack.dragon";
-                String s1 = s + ".player_" + new Random().nextInt(2);
-                return new TextComponentString(entityLivingBaseIn.getDisplayName().getFormattedText() + " ").appendSibling(new TextComponentTranslation(s1, entityLivingBaseIn.getDisplayName()));
-            }
-        };
-        dragonFire = new DamageSource("dragon_fire") {
-            @Override
-            public ITextComponent getDeathMessage(EntityLivingBase entityLivingBaseIn) {
-                String s = "death.attack.dragon_fire";
-                String s1 = s + ".player_" + new Random().nextInt(2);
-                return new TextComponentString(entityLivingBaseIn.getDisplayName().getFormattedText() + " ").appendSibling(new TextComponentTranslation(s1, entityLivingBaseIn.getDisplayName()));
-            }
-        }.setFireDamage();
-        dragonIce = new DamageSource("dragon_ice") {
-            @Override
-            public ITextComponent getDeathMessage(EntityLivingBase entityLivingBaseIn) {
-                String s = "death.attack.dragon_ice";
-                String s1 = s + ".player_" + new Random().nextInt(2);
-                return new TextComponentString(entityLivingBaseIn.getDisplayName().getFormattedText() + " ").appendSibling(new TextComponentTranslation(s1, entityLivingBaseIn.getDisplayName()));
-            }
-        };
-        gorgon = new DamageSource("gorgon") {
-            @Override
-            public ITextComponent getDeathMessage(EntityLivingBase entityLivingBaseIn) {
-                String s = "death.attack.gorgon";
-                String s1 = s + ".player_" + new Random().nextInt(2);
-                return new TextComponentString(entityLivingBaseIn.getDisplayName().getFormattedText() + " ").appendSibling(new TextComponentTranslation(s1, entityLivingBaseIn.getDisplayName()));
-            }
-        }.setDamageBypassesArmor();
     }
 
-    @EventHandler
-    public void postInit(FMLPostInitializationEvent event) {
-        PROXY.postRender();
-        TinkersCompatBridge.loadTinkersPostInitCompat();
-        IafRecipeRegistry.postInit();
-        logger.info("A brother bound to a love he must hide");
-        logger.info("The younger's armor is worn in the mind");
-        logger.info("A cold iron throne holds a boy barely grown");
-        logger.info("And now it is known");
-        logger.info("A claim to the prize, a crown laced in lies");
-        logger.info("You win or you die");
-        logger.info("Damn season 8 really sucked didn't it");
+    public static <MSG> void sendMSGToServer(MSG message) {
+        NETWORK_WRAPPER.sendToServer(message);
+    }
+
+    public static <MSG> void sendMSGToAll(MSG message) {
+        for (ServerPlayerEntity player : ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers()) {
+            sendNonLocal(message, player);
+        }
+    }
+
+    public static <MSG> void sendNonLocal(MSG msg, ServerPlayerEntity player) {
+        if (player.server.isDedicatedServer() || !player.getName().equals(player.server.getServerOwner())) {
+            NETWORK_WRAPPER.sendTo(msg, player.connection.netManager, NetworkDirection.PLAY_TO_CLIENT);
+        }
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void setupClient(FMLClientSetupEvent event) {
     }
 }
