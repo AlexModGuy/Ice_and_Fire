b7e1c93d2e2f1929ab06091083717a98708d9d19
Alex-the-666 <alex.rowlands@cox.net>
2020-11-19 13:40:00 -0800

    limit on ghost spawning

--- src/main/java/com/github/alexthe666/iceandfire/block/BlockGraveyardSoil.java
+++ src/main/java/com/github/alexthe666/iceandfire/block/BlockGraveyardSoil.java
@@ -11,6 +11,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.server.ServerWorld;
@@ -38,15 +39,19 @@ public class BlockGraveyardSoil extends Block {
             if (!worldIn.isAreaLoaded(pos, 3))
                 return;
             if (!worldIn.isDaytime() && !worldIn.getBlockState(pos.up()).isSolid() && rand.nextInt(9) == 0 && worldIn.getDifficulty() != Difficulty.PEACEFUL) {
-                EntityGhost ghost = IafEntityRegistry.GHOST.create(worldIn);
-                Random random = new Random();
-                ghost.setPositionAndRotation(pos.getX() + 0.5F, pos.getY() + 0.5F, pos.getZ() + 0.5F, random.nextFloat() * 360F, 0);
-                if (!worldIn.isRemote) {
-                    ghost.onInitialSpawn(worldIn, worldIn.getDifficultyForLocation(pos), SpawnReason.SPAWNER, null, null);
-                    worldIn.addEntity(ghost);
+                int checkRange = 32;
+                int k = worldIn.getEntitiesWithinAABB(EntityGhost.class, (new AxisAlignedBB(pos.getX(), pos.getY(), pos.getZ(), pos.getX() + 1, pos.getY() + 1, pos.getZ() + 1)).grow(checkRange)).size();
+                if(k < 10){
+                    EntityGhost ghost = IafEntityRegistry.GHOST.create(worldIn);
+                    Random random = new Random();
+                    ghost.setPositionAndRotation(pos.getX() + 0.5F, pos.getY() + 0.5F, pos.getZ() + 0.5F, random.nextFloat() * 360F, 0);
+                    if (!worldIn.isRemote) {
+                        ghost.onInitialSpawn(worldIn, worldIn.getDifficultyForLocation(pos), SpawnReason.SPAWNER, null, null);
+                        worldIn.addEntity(ghost);
+                    }
+                    ghost.setAnimation(EntityGhost.ANIMATION_SCARE);
+                    ghost.setHomePosAndDistance(pos, 16);
                 }
-                ghost.setAnimation(EntityGhost.ANIMATION_SCARE);
-                ghost.setHomePosAndDistance(pos, 16);
             }
         }
     }
