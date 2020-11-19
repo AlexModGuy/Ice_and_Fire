55578676d9fd56433ec8f7f1553338f6639eb401
Alex-the-666 <alex.rowlands@cox.net>
2020-11-19 13:37:35 -0800

    fix tide trident being normal trident

--- src/main/java/com/github/alexthe666/iceandfire/item/ItemTideTrident.java
+++ src/main/java/com/github/alexthe666/iceandfire/item/ItemTideTrident.java
@@ -19,6 +19,7 @@ import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -44,6 +45,70 @@ public class ItemTideTrident extends TridentItem {
         super(new Item.Properties().group(IceAndFire.TAB_ITEMS).maxDamage(400));
         this.setRegistryName(IceAndFire.MODID, "tide_trident");
     }
+
+    @Override
+    public void onPlayerStoppedUsing(ItemStack p_77615_1_, World p_77615_2_, LivingEntity p_77615_3_, int p_77615_4_) {
+        if (p_77615_3_ instanceof PlayerEntity) {
+            PlayerEntity lvt_5_1_ = (PlayerEntity)p_77615_3_;
+            int lvt_6_1_ = this.getUseDuration(p_77615_1_) - p_77615_4_;
+            if (lvt_6_1_ >= 10) {
+                int lvt_7_1_ = EnchantmentHelper.getRiptideModifier(p_77615_1_);
+                if (lvt_7_1_ <= 0 || lvt_5_1_.isWet()) {
+                    if (!p_77615_2_.isRemote) {
+                        p_77615_1_.damageItem(1, lvt_5_1_, (p_220047_1_) -> {
+                            p_220047_1_.sendBreakAnimation(p_77615_3_.getActiveHand());
+                        });
+                        if (lvt_7_1_ == 0) {
+                            EntityTideTrident lvt_8_1_ = new EntityTideTrident(p_77615_2_, lvt_5_1_, p_77615_1_);
+                            lvt_8_1_.func_234612_a_(lvt_5_1_, lvt_5_1_.rotationPitch, lvt_5_1_.rotationYaw, 0.0F, 2.5F + (float)lvt_7_1_ * 0.5F, 1.0F);
+                            if (lvt_5_1_.abilities.isCreativeMode) {
+                                lvt_8_1_.pickupStatus = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
+                            }
+
+                            p_77615_2_.addEntity(lvt_8_1_);
+                            p_77615_2_.playMovingSound((PlayerEntity)null, lvt_8_1_, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                            if (!lvt_5_1_.abilities.isCreativeMode) {
+                                lvt_5_1_.inventory.deleteStack(p_77615_1_);
+                            }
+                        }
+                    }
+
+                    lvt_5_1_.addStat(Stats.ITEM_USED.get(this));
+                    if (lvt_7_1_ > 0) {
+                        float lvt_8_2_ = lvt_5_1_.rotationYaw;
+                        float lvt_9_1_ = lvt_5_1_.rotationPitch;
+                        float lvt_10_1_ = -MathHelper.sin(lvt_8_2_ * 0.017453292F) * MathHelper.cos(lvt_9_1_ * 0.017453292F);
+                        float lvt_11_1_ = -MathHelper.sin(lvt_9_1_ * 0.017453292F);
+                        float lvt_12_1_ = MathHelper.cos(lvt_8_2_ * 0.017453292F) * MathHelper.cos(lvt_9_1_ * 0.017453292F);
+                        float lvt_13_1_ = MathHelper.sqrt(lvt_10_1_ * lvt_10_1_ + lvt_11_1_ * lvt_11_1_ + lvt_12_1_ * lvt_12_1_);
+                        float lvt_14_1_ = 3.0F * ((1.0F + (float)lvt_7_1_) / 4.0F);
+                        lvt_10_1_ *= lvt_14_1_ / lvt_13_1_;
+                        lvt_11_1_ *= lvt_14_1_ / lvt_13_1_;
+                        lvt_12_1_ *= lvt_14_1_ / lvt_13_1_;
+                        lvt_5_1_.addVelocity((double)lvt_10_1_, (double)lvt_11_1_, (double)lvt_12_1_);
+                        lvt_5_1_.startSpinAttack(20);
+                        if (lvt_5_1_.func_233570_aj_()) {
+                            float lvt_15_1_ = 1.1999999F;
+                            lvt_5_1_.move(MoverType.SELF, new Vector3d(0.0D, 1.1999999284744263D, 0.0D));
+                        }
+
+                        SoundEvent lvt_15_4_;
+                        if (lvt_7_1_ >= 3) {
+                            lvt_15_4_ = SoundEvents.ITEM_TRIDENT_RIPTIDE_3;
+                        } else if (lvt_7_1_ == 2) {
+                            lvt_15_4_ = SoundEvents.ITEM_TRIDENT_RIPTIDE_2;
+                        } else {
+                            lvt_15_4_ = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
+                        }
+
+                        p_77615_2_.playMovingSound((PlayerEntity)null, lvt_5_1_, lvt_15_4_, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                    }
+
+                }
+            }
+        }
+    }
+
 
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
