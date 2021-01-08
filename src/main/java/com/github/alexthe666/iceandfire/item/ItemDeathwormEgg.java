5ecf2142bc0e04e35f85b04b7b6e5c4235486d39
BiohazardStuff <BiohazardStuff@users.noreply.github.com>
2021-01-08 19:09:17 +1000

    Correct Deathworm egg throw method to be consistent with other eggs. Resolves Deathworm egg being launched at completely the wrong angle. #3328

--- src/main/java/com/github/alexthe666/iceandfire/item/ItemDeathwormEgg.java
+++ src/main/java/com/github/alexthe666/iceandfire/item/ItemDeathwormEgg.java
@@ -35,7 +35,7 @@ public class ItemDeathwormEgg extends Item implements ICustomRendered {
 
         if (!worldIn.isRemote) {
             EntityDeathWormEgg entityegg = new EntityDeathWormEgg(IafEntityRegistry.DEATH_WORM_EGG, playerIn, worldIn, gigantic);
-            entityegg.shoot( playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
+            entityegg.func_234612_a_(playerIn, playerIn.rotationPitch, playerIn.rotationYaw, 0.0F, 1.5F, 1.0F);
             worldIn.addEntity(entityegg);
         }
 
