ffe62807429139ac6b574a957e2d179679aab30d
Matyrobbrt <matyrobbrt@gmail.com>
2022-04-30 15:59:42 +0300

    Improve GhostAI

--- src/main/java/com/github/alexthe666/iceandfire/entity/ai/FlyingAITarget.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/ai/FlyingAITarget.java
@@ -1,25 +1,28 @@
 package com.github.alexthe666.iceandfire.entity.ai;
 
+import java.util.function.Predicate;
+
 import javax.annotation.Nullable;
 
 import com.github.alexthe666.iceandfire.entity.EntitySeaSerpent;
-import com.google.common.base.Predicate;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
 import net.minecraft.util.math.AxisAlignedBB;
 
-public class FlyingAITarget extends NearestAttackableTargetGoal {
+public class FlyingAITarget extends NearestAttackableTargetGoal<LivingEntity> {
 
-    public FlyingAITarget(MobEntity creature, Class classTarget, boolean checkSight) {
+    public FlyingAITarget(MobEntity creature, Class<LivingEntity> classTarget, boolean checkSight) {
         super(creature, classTarget, checkSight);
     }
 
-    public FlyingAITarget(MobEntity creature, Class classTarget, boolean checkSight, boolean onlyNearby) {
+    public FlyingAITarget(MobEntity creature, Class<LivingEntity> classTarget, boolean checkSight, boolean onlyNearby) {
         super(creature, classTarget, checkSight, onlyNearby);
     }
 
-    public FlyingAITarget(MobEntity creature, Class classTarget, int chance, boolean checkSight, boolean onlyNearby, @Nullable final Predicate targetSelector) {
+    public FlyingAITarget(MobEntity creature, Class<LivingEntity> classTarget, int chance, boolean checkSight,
+        boolean onlyNearby, @Nullable final Predicate<LivingEntity> targetSelector) {
         super(creature, classTarget, chance, checkSight, onlyNearby, targetSelector);
     }
 
@@ -28,6 +31,7 @@ public class FlyingAITarget extends NearestAttackableTargetGoal {
         return this.goalOwner.getBoundingBox().grow(targetDistance, targetDistance, targetDistance);
     }
 
+    @Override
     public boolean shouldExecute() {
         if (goalOwner instanceof EntitySeaSerpent && (((EntitySeaSerpent) goalOwner).isJumpingOutOfWater() || !goalOwner.isInWater())) {
             return false;
