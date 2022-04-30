e44ee9cf93b781a976b5888c32bc5b633e8e4179
Paint_Ninja <PaintNinja@users.noreply.github.com>
2022-04-30 15:47:41 +0100

    Implement AABB cache for AmphithereAIFleePlayer
    Chances are that the player isn't going be moving much further away within 0.2s, so don't recalculate a new list every tick (0.05s).

--- src/main/java/com/github/alexthe666/iceandfire/util/IAFMath.java
+++ src/main/java/com/github/alexthe666/iceandfire/util/IAFMath.java
@@ -1,12 +1,20 @@
 package com.github.alexthe666.iceandfire.util;
 
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+
+import java.util.Collections;
+import java.util.List;
+
 public class IAFMath {
 
+    public static final List<PlayerEntity> emptyPlayerEntityList = Collections.emptyList();
+    public static final List<ItemEntity> emptyItemEntityList = Collections.emptyList();
     private static final double coeff_1 = Math.PI / 4;
     private static final double coeff_2 = coeff_1 * 3;
 
-    public static final double atan2_accurate(double y, double x) {
-        double r;
+    public static double atan2_accurate(double y, double x) {
+        final double r;
         if (y < 0) {
             y = -y;
             if (x > 0) {
