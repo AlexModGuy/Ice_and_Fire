f645106c56cf70e24dec834e721efac464234dcf
TheBv <thebv939@gmail.com>
2022-05-02 15:23:55 +0200

    Fixed the tide trident not piercing targets. Fixes #4022 Fixed the item model not showing the enchantment glow. The tide trident is now affected by the piercing enchantment.

--- src/main/java/com/github/alexthe666/iceandfire/item/ItemTideTrident.java
+++ src/main/java/com/github/alexthe666/iceandfire/item/ItemTideTrident.java
@@ -1,17 +1,12 @@
 package com.github.alexthe666.iceandfire.item;
 
-import java.util.List;
-
-import javax.annotation.Nullable;
-
 import com.github.alexthe666.iceandfire.IceAndFire;
 import com.github.alexthe666.iceandfire.entity.EntityTideTrident;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
-
-import net.minecraft.block.BlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.PiercingEnchantment;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MoverType;
 import net.minecraft.entity.ai.attributes.Attribute;
@@ -19,25 +14,23 @@ import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
-import net.minecraft.entity.projectile.TridentEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
-import net.minecraft.item.UseAction;
 import net.minecraft.stats.Stats;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemTideTrident extends TridentItem {
 
@@ -49,7 +42,7 @@ public class ItemTideTrident extends TridentItem {
     @Override
     public void onPlayerStoppedUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
         if (entityLiving instanceof PlayerEntity) {
-            PlayerEntity lvt_5_1_ = (PlayerEntity)entityLiving;
+            PlayerEntity lvt_5_1_ = (PlayerEntity) entityLiving;
             int lvt_6_1_ = this.getUseDuration(stack) - timeLeft;
             if (lvt_6_1_ >= 10) {
                 int lvt_7_1_ = EnchantmentHelper.getRiptideModifier(stack);
@@ -60,13 +53,13 @@ public class ItemTideTrident extends TridentItem {
                         });
                         if (lvt_7_1_ == 0) {
                             EntityTideTrident lvt_8_1_ = new EntityTideTrident(worldIn, lvt_5_1_, stack);
-                            lvt_8_1_.setDirectionAndMovement(lvt_5_1_, lvt_5_1_.rotationPitch, lvt_5_1_.rotationYaw, 0.0F, 2.5F + (float)lvt_7_1_ * 0.5F, 1.0F);
+                            lvt_8_1_.setDirectionAndMovement(lvt_5_1_, lvt_5_1_.rotationPitch, lvt_5_1_.rotationYaw, 0.0F, 2.5F + (float) lvt_7_1_ * 0.5F, 1.0F);
                             if (lvt_5_1_.abilities.isCreativeMode) {
                                 lvt_8_1_.pickupStatus = AbstractArrowEntity.PickupStatus.CREATIVE_ONLY;
                             }
 
                             worldIn.addEntity(lvt_8_1_);
-                            worldIn.playMovingSound((PlayerEntity)null, lvt_8_1_, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                            worldIn.playMovingSound(null, lvt_8_1_, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F);
                             if (!lvt_5_1_.abilities.isCreativeMode) {
                                 lvt_5_1_.inventory.deleteStack(stack);
                             }
@@ -81,11 +74,11 @@ public class ItemTideTrident extends TridentItem {
                         float lvt_11_1_ = -MathHelper.sin(lvt_9_1_ * 0.017453292F);
                         float lvt_12_1_ = MathHelper.cos(lvt_8_2_ * 0.017453292F) * MathHelper.cos(lvt_9_1_ * 0.017453292F);
                         float lvt_13_1_ = MathHelper.sqrt(lvt_10_1_ * lvt_10_1_ + lvt_11_1_ * lvt_11_1_ + lvt_12_1_ * lvt_12_1_);
-                        float lvt_14_1_ = 3.0F * ((1.0F + (float)lvt_7_1_) / 4.0F);
+                        float lvt_14_1_ = 3.0F * ((1.0F + (float) lvt_7_1_) / 4.0F);
                         lvt_10_1_ *= lvt_14_1_ / lvt_13_1_;
                         lvt_11_1_ *= lvt_14_1_ / lvt_13_1_;
                         lvt_12_1_ *= lvt_14_1_ / lvt_13_1_;
-                        lvt_5_1_.addVelocity((double)lvt_10_1_, (double)lvt_11_1_, (double)lvt_12_1_);
+                        lvt_5_1_.addVelocity(lvt_10_1_, lvt_11_1_, lvt_12_1_);
                         lvt_5_1_.startSpinAttack(20);
                         if (lvt_5_1_.isOnGround()) {
                             float lvt_15_1_ = 1.1999999F;
@@ -101,7 +94,7 @@ public class ItemTideTrident extends TridentItem {
                             lvt_15_4_ = SoundEvents.ITEM_TRIDENT_RIPTIDE_1;
                         }
 
-                        worldIn.playMovingSound((PlayerEntity)null, lvt_5_1_, lvt_15_4_, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                        worldIn.playMovingSound(null, lvt_5_1_, lvt_15_4_, SoundCategory.PLAYERS, 1.0F, 1.0F);
                     }
 
                 }
@@ -113,13 +106,19 @@ public class ItemTideTrident extends TridentItem {
     public Multimap<Attribute, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
         if (equipmentSlot == EquipmentSlotType.MAINHAND) {
-            builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)12.0D, AttributeModifier.Operation.ADDITION));
-            builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", (double)-2.9F, AttributeModifier.Operation.ADDITION));
+            builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", 12.0D, AttributeModifier.Operation.ADDITION));
+            builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2.9F, AttributeModifier.Operation.ADDITION));
         }
 
         return builder.build();
     }
 
+    @Override
+    public boolean canApplyAtEnchantingTable(ItemStack stack, net.minecraft.enchantment.Enchantment enchantment) {
+        if (enchantment instanceof PiercingEnchantment)
+            return true;
+        return enchantment.type.canEnchantItem(stack.getItem());
+    }
 
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
