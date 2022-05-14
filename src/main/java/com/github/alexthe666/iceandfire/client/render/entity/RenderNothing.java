e7b654faab182c032b8bf39ed3071eea3d0640f9
TheBv <thebv939@gmail.com>
2022-05-14 22:27:23 +0200

    RenderNothing now only gets rendered when debug bounding boxes are enabled. Hopefully fixes #3807

--- src/main/java/com/github/alexthe666/iceandfire/client/render/entity/RenderNothing.java
+++ src/main/java/com/github/alexthe666/iceandfire/client/render/entity/RenderNothing.java
@@ -3,25 +3,35 @@ package com.github.alexthe666.iceandfire.client.render.entity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.culling.ClippingHelper;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class RenderNothing extends EntityRenderer {
+public class RenderNothing<T extends Entity> extends EntityRenderer<T> {
 
     public RenderNothing(EntityRendererManager renderManager) {
         super(renderManager);
     }
 
-    public void render(Entity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
+    public void render(T entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
         super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
-        
     }
-        @Override
+
+    // Only render if the debug bboxes are enabled
+    @Override
+    public boolean shouldRender(T livingEntityIn, ClippingHelper camera, double camX, double camY, double camZ) {
+        if (!this.renderManager.isDebugBoundingBox())
+            return false;
+        return super.shouldRender(livingEntityIn, camera, camX, camY, camZ);
+    }
+
+    @Override
     public ResourceLocation getEntityTexture(Entity entity) {
         return null;
     }
