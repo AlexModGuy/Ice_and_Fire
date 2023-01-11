0703ff01c61c7155aece8425a017e241a0f05507
TheBv <thebv939@gmail.com>
2023-01-11 13:46:59 +0100

    Fixed crash/issue related to graveyards

--- src/main/java/com/github/alexthe666/iceandfire/world/structure/Pool.java
+++ src/main/java/com/github/alexthe666/iceandfire/world/structure/Pool.java
@@ -10,7 +10,7 @@ import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 
 public class Pool {
     static final ResourceKey<StructureTemplatePool> dread_pool = ResourceKey.create(BuiltinRegistries.TEMPLATE_POOL.key(), new ResourceLocation(IceAndFire.MODID, "dread_mausoleum/start_pool"));
-    static final ResourceKey<StructureTemplatePool> graveyard_pool = ResourceKey.create(BuiltinRegistries.TEMPLATE_POOL.key(), new ResourceLocation(IceAndFire.MODID, "graveyard/start_pool"));
+    static final ResourceKey<StructureTemplatePool> graveyard_pool = ResourceKey.create(BuiltinRegistries.TEMPLATE_POOL.key(), new ResourceLocation(IceAndFire.MODID, "graveyard/top_pool"));
     static final ResourceKey<StructureTemplatePool> gorgon_pool = ResourceKey.create(BuiltinRegistries.TEMPLATE_POOL.key(), new ResourceLocation(IceAndFire.MODID, "gorgon_temple/top_pool"));
 
     public static PieceGeneratorSupplier.Context<JigsawConfiguration> replaceContext(PieceGeneratorSupplier.Context<JigsawConfiguration> context, JigsawConfiguration configuration) {
