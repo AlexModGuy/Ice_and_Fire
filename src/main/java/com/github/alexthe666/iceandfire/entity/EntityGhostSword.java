4336d9396f08283d2556b6f92e5c6bf97f99b7c0
adam9899 <57104144+adam9899@users.noreply.github.com>
2023-01-22 17:03:05 +0000

    Buff Ghost Sword Projectile -
    Currently the Ghost Sword does not do a whole lot of damage as a projectile, the weapon is very fun to use but could be stronger. 
    At the moment it is unwise to use the projectile over melee.
    I have increased its damage by 40%ish.

--- src/main/java/com/github/alexthe666/iceandfire/entity/EntityGhostSword.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/EntityGhostSword.java
@@ -30,14 +30,14 @@ public class EntityGhostSword extends AbstractArrowEntity {
 
     public EntityGhostSword(EntityType<? extends AbstractArrowEntity> type, World worldIn) {
         super(type, worldIn);
-        this.setDamage(9F);
+        this.setDamage(13F);
     }
 
     public EntityGhostSword(EntityType<? extends AbstractArrowEntity> type, World worldIn, double x, double y, double z,
                             float r, float g, float b) {
         this(type, worldIn);
         this.setPosition(x, y, z);
-        this.setDamage(9F);
+        this.setDamage(13F);
     }
 
     public EntityGhostSword(EntityType<? extends AbstractArrowEntity> type, World worldIn, LivingEntity shooter,
