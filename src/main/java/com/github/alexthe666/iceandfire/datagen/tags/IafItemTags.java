900e3811fa938c42f858c1d98ff1b31f5623feee
Kai <kai.zahn@yahoo.de>
2024-01-21 12:13:00 +0100

    actually add tags

--- src/main/java/com/github/alexthe666/iceandfire/datagen/tags/IafItemTags.java
+++ src/main/java/com/github/alexthe666/iceandfire/datagen/tags/IafItemTags.java
@@ -258,19 +258,19 @@ public class IafItemTags extends ItemTagsProvider {
                 tag(ItemTags.ARROWS).add(item);
             } else if (item instanceof SwordItem) {
                 tag(Tags.Items.TOOLS).add(item);
-                tag(ItemTags.SWORDS);
+                tag(ItemTags.SWORDS).add(item);
             } else if (item instanceof PickaxeItem) {
                 tag(Tags.Items.TOOLS).add(item);
-                tag(ItemTags.PICKAXES);
+                tag(ItemTags.PICKAXES).add(item);
             } else if (item instanceof AxeItem) {
                 tag(Tags.Items.TOOLS).add(item);
-                tag(ItemTags.AXES);
+                tag(ItemTags.AXES).add(item);
             } else if (item instanceof ShovelItem) {
                 tag(Tags.Items.TOOLS).add(item);
-                tag(ItemTags.SHOVELS);
+                tag(ItemTags.SHOVELS).add(item);
             } else if (item instanceof HoeItem) {
                 tag(Tags.Items.TOOLS).add(item);
-                tag(ItemTags.HOES);
+                tag(ItemTags.HOES).add(item);
             } else if (item instanceof BowItem) {
                 tag(Tags.Items.TOOLS_BOWS).add(item);
             } else if (item instanceof TridentItem) {
