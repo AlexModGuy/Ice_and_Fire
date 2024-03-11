1c66c25c01f9dd91ad3cc57ac0624ebc55ed4c27
Kai <kai.zahn@yahoo.de>
2024-03-11 19:46:17 +0100

    log summon command

--- src/main/java/com/github/alexthe666/iceandfire/entity/util/EntityUtil.java
+++ src/main/java/com/github/alexthe666/iceandfire/entity/util/EntityUtil.java
@@ -1,12 +1,18 @@
 package com.github.alexthe666.iceandfire.entity.util;
 
 import com.github.alexthe666.iceandfire.IceAndFire;
+import com.github.alexthe666.iceandfire.entity.EntityDragonBase;
 import com.github.alexthe666.iceandfire.entity.EntityMutlipartPart;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+import java.util.UUID;
 
 public class EntityUtil {
     public static void updatePart(@Nullable final EntityMutlipartPart part, @NotNull final LivingEntity parent) {
@@ -24,6 +30,20 @@ public class EntityUtil {
             }
 
             if (attempts == 0 && !part.isAddedToWorld()) {
+                if (parent instanceof EntityDragonBase dragon) {
+                    ResourceLocation location = Objects.requireNonNull(ForgeRegistries.ENTITY_TYPES.getKey(dragon.getType()));
+                    UUID owner = dragon.getOwnerUUID();
+
+                    String command = "/summon " + location + " ~ ~ ~ {AgeInTicks:" + dragon.getAgeInTicks() + ",Variant:" + dragon.getVariant() + ",Gender:" + dragon.isMale();
+
+                    if (owner != null) {
+                        command += ",TamedDragon:true,Owner:" + owner;
+                    }
+
+                    command += "}";
+                    IceAndFire.LOGGER.info("You can re-summon the dragon with [{}]", command);
+                }
+
                 parent.discard();
                 IceAndFire.LOGGER.error("Discarded [{}] due to constant UUID clash", parent);
             } else {
